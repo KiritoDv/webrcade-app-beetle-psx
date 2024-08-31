7bdcbe24795f7cb73b96689aa7fd0f0a416ef87c
KiritoDv <kiritodev01@gmail.com>
2024-08-30 19:03:43 -0600

    Moved to use openbios

--- src/App.js
+++ src/App.js
@@ -13,16 +13,18 @@ class App extends WebrcadeRetroApp {
 
   getBiosMap() {
     return {
-      '8dd7d5296a650fac7319bce665a6a53c': 'scph5500.bin', // JPN
-      '490f666e1afb15b7362b406ed1cea246': 'scph5501.bin', // USA
-      '32736f17079d0b2b7024407c39bd3050': 'scph5502.bin', // EUR
+      '8dd7d5296a650fac7319bce665a6a53c': 'openbios.bin', // JPN
+      '490f666e1afb15b7362b406ed1cea246': 'openbios.bin', // USA
+      '32736f17079d0b2b7024407c39bd3050': 'openbios.bin', // EUR
+      'f1c424ef00d07d21fefa48507f021dc6': 'openbios.bin', // OBIOS
     }
   }
 
   getAlternateBiosMap() {
     return {
-      'c53ca5908936d412331790f4426c6c33': 'PSXONPSP660.bin',
-      '81bbe60ba7a3d1cea1d48c14cbcc647b': 'ps1_rom.bin',
+      'c53ca5908936d412331790f4426c6c33': 'openbios.bin',
+      '81bbe60ba7a3d1cea1d48c14cbcc647b': 'openbios.bin',
+      'f1c424ef00d07d21fefa48507f021dc6': 'openbios.bin', // OBIOS
     };
   }
 
